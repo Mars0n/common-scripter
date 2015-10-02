bf4a8decdf0da0c0930e8cdc6f46e1e95b4a0657
marsn <mpuccio90@gmail.com>
2015-10-01 23:51:40 -0300

    file comments

--- common_starter.sh
+++ common_starter.sh
@@ -1,5 +1,41 @@
-read -r -p "Nombre del proyecto: " project_name
-read -r -p "Nombre de archivo setting: " setting_name
+#!/bin/bash
 
-echo $project_name
-echo $setting_name
+# Read the settings directory name and the custom setting file  
+read -r -p "Project setting directory name: " project_name
+read -r -p "Custom setting file name: " setting_name
+
+# Scripts
+echo python manage.py runserver --settings=$project_name.$setting_name > run.sh
+echo python manage.py shell --settings=$project_name.$setting_name > shell.sh
+echo python manage.py dbshell --settings=$project_name.$setting_name > dbshell.sh
+
+# Execute mode on files
+chmod +x run.sh shell.sh dbshell.sh
+
+#Create a .gitignore file in a superior directory for common django project
+cat <<EOF>> ../.gitignore
+*.pyc
+*.swn
+*.swo
+*.swp
+
+$project_name/$setting_name.py
+run.sh
+shell.sh
+dbshell.sh
+db.sqlit3
+
+css/
+stylesheets/
+bower_components/
+
+.sass_cache
+.DS_Store
+Gemfile
+Gemfile.lock
+EOF
+
+
+
+
+
